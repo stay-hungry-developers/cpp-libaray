4e320256ca8690b767418b5d626d609e9a58dd6e
LeeHemingway <coherence0815@gmail.com>
2021-10-19 14:10:25 +0900

    init project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cpp-libaray
+
+* `-std=c++0x` must be in build script in order to use `unordered_map`
+
+
+
---
+++ demo/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <header.hpp>
+using namespace std;
+
+int main() {
+  std::cout << "in main" << std::endl;
+  
+  Cls cls;
+  cls.SayHello();
+
+  return 0;
+}
+
+
